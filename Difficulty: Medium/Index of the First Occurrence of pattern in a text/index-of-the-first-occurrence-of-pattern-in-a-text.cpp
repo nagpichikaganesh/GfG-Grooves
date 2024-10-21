92c22914424318f270a7807bcdbf7f543ae1941f
Pichika Ganesh <nagpichikaganesh@gmail.com>
2024-10-21 23:50:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Index of the First Occurrence of pattern in a text/index-of-the-first-occurrence-of-pattern-in-a-text.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int findMatching(string text, string pat) {
+        // Code here
+        if(pat.empty()){
+            return -1;
+        }
+        
+        return text.find(pat);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string text, pat;
+        cin >> text >> pat;
+        Solution obj;
+        cout << obj.findMatching(text, pat) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
